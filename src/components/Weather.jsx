125bf215b2db52583a0df8d50df8021f27871aa6
SREERAJ K <sreerajkadukassery@gmail.com>
2024-06-25 18:53:45 +0530

    First Commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Weather.css'
+import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
+import humidity_icon from '../assets/humidity.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
+import wind_icon from '../assets/wind.png'
+const Weather = () => {
+  return (
+    <div className='weather'>
+      <div className='search-bar'> 
+        <input type="text" placeholder='Search' />
+        <img src={search_icon} alt="" />
+      </div>
+      <img src={clear_icon} alt=""  className='weather-icon'/>
+      <p className='temp'>16°C</p>
+      <p className='loc'>London</p>
+      <div className='weather-data'>
+        <div className="col">
+            <img src={humidity_icon} alt="" />
+            <div>
+                <p>91%</p>
+                <span>Humidity</span>
+            </div>
+        </div>
+        <div className="col">
+            <img src={wind_icon} alt="" />
+            <div>
+                <p>9.1Km/h</p>
+                <span>Speed</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Weather
